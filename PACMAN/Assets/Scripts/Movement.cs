8a97450a962a6b241e72c318caae31207fb7d516
Konrad Krawczyk <56159654+KonradK28@users.noreply.github.com>
2022-05-13 08:07:46 +0200

    Motion addrd to PacMan

--- PACMAN/Assets/Scripts/Movement.cs
+++ PACMAN/Assets/Scripts/Movement.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody))]
 public class Movement : MonoBehaviour
 {
     public float speed = 8f;
     public float speedMultiplier = 1f;
-    public Vector2 initialDirection;
+    public Vector3 initialDirection;
     public LayerMask obstacleLayer;
 
-    public new Rigidbody2D rigidbody { get; private set; }
-    public Vector2 direction { get; private set; }
-    public Vector2 nextDirection { get; private set; }
+    public new Rigidbody rigidbody { get; private set; }
+    public Vector3 direction { get; private set; }
+    public Vector3 nextDirection { get; private set; }
     public Vector3 startingPosition { get; private set; }
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody>();
         startingPosition = transform.position;
     }
 
@@ -28,50 +28,25 @@ public class Movement : MonoBehaviour
     {
         speedMultiplier = 1f;
         direction = initialDirection;
-        nextDirection = Vector2.zero;
+        nextDirection = Vector3.zero;
         transform.position = startingPosition;
         rigidbody.isKinematic = false;
-        enabled = true;
+        //enabled = true;
     }
 
     private void Update()
     {
-        // Try to move in the next direction while it's queued to make movements
-        // more responsive
-        if (nextDirection != Vector2.zero) {
-            SetDirection(nextDirection);
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        Vector2 position = rigidbody.position;
-        Vector2 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
+        Vector3 position = rigidbody.position;
+        Vector3 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
 
         rigidbody.MovePosition(position + translation);
     }
 
-    public void SetDirection(Vector2 direction, bool forced = false)
+    public void SetDirection(Vector3 direction, bool forced = false)
     {
-        // Only set the direction if the tile in that direction is available
-        // otherwise we set it as the next direction so it'll automatically be
-        // set when it does become available
-        if (forced || !Occupied(direction))
-        {
-            this.direction = direction;
-            nextDirection = Vector2.zero;
-        }
-        else
-        {
-            nextDirection = direction;
-        }
-    }
-
-    public bool Occupied(Vector2 direction)
-    {
-        // If no collider is hit then there is no obstacle in that direction
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, direction, 1.5f, obstacleLayer);
-        return hit.collider != null;
+        this.direction = direction;
+        nextDirection = Vector3.zero;
+        
     }
 
 }
